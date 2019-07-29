05aeb96704f25beb7c1e0c3719e83f761ba91283
Takumi <takumi.dawn@gmail.com>
2019-07-29 18:17:08 -0500

    all basic page layouts

---
+++ app/src/main/java/edu/illinois/cs411/godutch/parsePicture.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+/**
+ * @author RickyDiamaidO
+ *
+ */
+package edu.illinois.cs411.godutch;
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+public class parsePicture {
+    // **********************************************
+    // *** Update or verify the following values. ***
+    // **********************************************
+
+    // Replace <Subscription Key> with your valid subscription key.
+    private static final String subscriptionKey = "e23898c9c61648cfbb30ec7091e335f6";
+
+    // You must use the same Azure region in your REST API method as you used to
+    // get your subscription keys. For example, if you got your subscription keys
+    // from the West US region, replace "westcentralus" in the URL
+    // below with "westus".
+    //
+    // Free trial subscription keys are generated in the "westus" region.
+    // If you use a free trial subscription key, you shouldn't need to change
+    // this region.
+    private static final String uriBase =
+            "https://westus.api.cognitive.microsoft.com/vision/v2.0/ocr";
+
+    private static final String imageToAnalyze =
+        "https://upload.cc/i1/2019/07/28/ORbIXA.jpeg";
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        try {
+            URIBuilder uriBuilder = new URIBuilder(uriBase);
+
+            uriBuilder.setParameter("language", "unk");
+            uriBuilder.setParameter("detectOrientation", "true");
+            uriBuilder.setParameter("isTable", "true");
+            uriBuilder.setParameter("scale", "true");
+
+            // Request parameters.
+            URI uri = uriBuilder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"" + imageToAnalyze + "\"}");
+            request.setEntity(requestEntity);
+
+            // Call the REST API method and get the response entity.
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("REST Response:\n");
+                System.out.println(json.toString(2));
+            }
+        } catch (Exception e) {
+            // Display error message.
+            System.out.println(e.getMessage());
+        }
+    }
+}
